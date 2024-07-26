17265e051b76206fea387fc2a83732b58f18d024
차차 <117566375+mr-chacha@users.noreply.github.com>
2024-07-26 12:04:10 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import { Viewer } from "resium";
 import "cesium/Build/Cesium/Widgets/widgets.css";
 import { Ion } from "cesium";
 
-Ion.defaultAccessToken =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiZDM4YThjZi1jMTVhLTRlN2ItYTFhNi0wNTA0MWMzMDhmNWEiLCJpZCI6MjMwNDU5LCJpYXQiOjE3MjE4MTI0MzB9.IaT-OLUu3bForPze6C6M_6xCLiY_I43k3PZQ4h-IrgA";
+Ion.defaultAccessToken =process.env.REACT_APP_CESIUM_TOKEN
 
 function App() {
   return (
